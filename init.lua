0849fbd9538504c98e5ea564cb6b41aaf8b3f8ca
DreamMaoMao <2523610504@qq.com>
2024-06-18 20:13:59 +0800

    optimize hovered file icon

--- init.lua
+++ init.lua
@@ -445,7 +445,7 @@ local toggle_ui = ya.sync(function(st)
 	File.icon = function(self, file)
 
 		local icon = file:icon()
-		local span_icon_before = ui.Span(" " .. file:icon().text .. " "):style(icon.style)
+		local span_icon_before = file:is_hovered() and ui.Span(" " .. file:icon().text .. " ") or ui.Span(" " .. file:icon().text .. " "):style(icon.style)
 		
 		if st.type == "global" then
 			local pos, view = rel_position(file, "all")
